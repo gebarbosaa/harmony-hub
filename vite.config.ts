b21cea7f20f90d35a9ad519ea23c150454222d55
Geovanna Barbosa <geovannab177@gmail.com>
2026-08-19 15:45:48 -0300

    Configure Rollup manual chunks for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000, // Aumenta o limite para 1 MB
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          charts: ["recharts"],
+        },
+      },
+    },
   },
 });
