c8950fb615c741caa759358095638a50705f5f16
Geovanna Barbosa <geovannab177@gmail.com>
2026-09-05 20:57:03 -0300

    fix: make painel route redirect to central panel

---
+++ src/routes/painel.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/painel")({
+  beforeLoad: () => {
+    throw redirect({ to: "/mais", replace: true });
+  },
+});
