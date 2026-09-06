e55ca4bc4535ca70692683200e39dc01cdf5ded1
Geovanna Barbosa <geovannab177@gmail.com>
2026-09-06 00:27:02 -0300

    fix: add official ajustes route alias

---
+++ src/routes/ajustes.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/ajustes")({
+  beforeLoad: () => { throw redirect({ to: "/configuracoes", replace: true }); },
+});
