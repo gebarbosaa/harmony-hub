4fbc8b7747f0820cb872f7c5bd5dfbf92507bfeb
Geovanna Barbosa <geovannab177@gmail.com>
2026-09-06 00:22:42 -0300

    fix: enable SSR for application routes

--- src/start.ts
+++ src/start.ts
@@ -28,5 +28,5 @@ const csrfMiddleware = createCsrfMiddleware({
 export const startInstance = createStart(() => ({
   functionMiddleware: [attachSupabaseAuth],
   requestMiddleware: [errorMiddleware, csrfMiddleware],
-  defaultSsr: false,
+  defaultSsr: true,
 }));
